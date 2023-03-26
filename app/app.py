d30066019a9cfa6dbe5e239af313d99f9e8f7daf
HP <alperengozeten@gmail.com>
2023-03-26 16:09:17 +0300

    implement logout

--- app/app.py
+++ app/app.py
@@ -73,6 +73,14 @@ def tasks():
 def analysis():
     return render_template('analysis.html')
 
+@app.route("/logout")
+def logout():
+    session["username"] = None
+    session['loggedin'] = False
+    session['userid'] = None
+    session['email'] = None
+    return redirect("/")
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
